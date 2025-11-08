fdd3a4933c8ce0c237b5b6dd3ce3f2bc6a31b1a6
varshavskiy_png <timikinvarshav05@gmail.com>
2025-11-09 00:33:27 +0200

    chore

---
+++ app/whishly/app.config.ts
@@ -0,0 +1,10 @@
+export default {
+    expo: {
+      name: "Whishly",
+      slug: "whishly",
+      version: "1.0.0",
+      extra: {
+        BASE_URL: process.env.BASE_URL || "http://localhost:4000"
+      }
+    }
+  };
---
+++ app/whishly/src/core/api/client.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+import Constants from "expo-constants";
+
+const BASE_URL = Constants.expoConfig?.extra?.BASE_URL || "http://localhost:4000";
+
+
+export const apiClient = axios.create({
+    baseURL: BASE_URL,
+    timeout: 5000,
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+apiClient.interceptors.response.use(async (config) => {
+    const token = 'mock-token';
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    
+    return config;
+})
